9c939274a156a20928c2789b48b1de59bd2cf478
Diane Nacor <nacordiane55@gmail.com>
2023-07-07 21:22:03 +0800

    Photo Studio

---
+++ script.js
@@ -0,0 +1,16 @@
+
+// window.addEventListener('scroll', function(){
+// 	const header = document.querySelector('navmenu');
+// 	header.classList.toggle("sticky", window.scrollY > 0);
+// });
+
+function toggleMenu(){
+	const menuToggle = document.querySelector('bx-menu');
+	const navigation = document.querySelector('.toggle');
+	
+	navigation.classList.toggle('active')
+}
+
+
+
+
